52da5e967f0749a96cc1a8a7022621d6f4e1632f
alekbori <aleksejs.borisjuks@tele2.com>
2016-07-08 14:50:20 +0300

    updated to nw@0.15.4

--- build.bat
+++ build.bat
@@ -37,11 +37,18 @@ cd ..\..
 echo.
 call :ColorText 0a "Copying files..."
 echo.
-if not exist %CUR_DIR%\release\ffmpegsumo.dll copy %CUR_DIR%\buildTools\nw\ffmpegsumo.dll %CUR_DIR%\release\ffmpegsumo.dll
-if not exist %CUR_DIR%\release\icudt.dll copy %CUR_DIR%\buildTools\nw\icudt.dll %CUR_DIR%\release\icudt.dll
+if not exist %CUR_DIR%\release\locales\ xcopy %CUR_DIR%\buildTools\nw\locales %CUR_DIR%\release\locales\
+if not exist %CUR_DIR%\release\ffmpeg.dll copy %CUR_DIR%\buildTools\nw\ffmpeg.dll %CUR_DIR%\release\ffmpeg.dll
+if not exist %CUR_DIR%\release\icudtl.dat copy %CUR_DIR%\buildTools\nw\icudtl.dat %CUR_DIR%\release\icudtl.dat
 if not exist %CUR_DIR%\release\libEGL.dll copy %CUR_DIR%\buildTools\nw\libEGL.dll %CUR_DIR%\release\libEGL.dll
 if not exist %CUR_DIR%\release\libGLESv2.dll copy %CUR_DIR%\buildTools\nw\libGLESv2.dll %CUR_DIR%\release\libGLESv2.dll
-if not exist %CUR_DIR%\release\nw.pak copy %CUR_DIR%\buildTools\nw\nw.pak %CUR_DIR%\release\nw.pak
+if not exist %CUR_DIR%\release\natives_blob.bin copy %CUR_DIR%\buildTools\nw\natives_blob.bin %CUR_DIR%\release\natives_blob.bin
+if not exist %CUR_DIR%\release\node.dll copy %CUR_DIR%\buildTools\nw\node.dll %CUR_DIR%\release\node.dll
+if not exist %CUR_DIR%\release\nw.dll copy %CUR_DIR%\buildTools\nw\nw.dll %CUR_DIR%\release\nw.dll
+if not exist %CUR_DIR%\release\nw_100_percent.pak copy %CUR_DIR%\buildTools\nw\nw_100_percent.pak %CUR_DIR%\release\nw_100_percent.pak
+if not exist %CUR_DIR%\release\nw_200_percent.pak copy %CUR_DIR%\buildTools\nw\nw_200_percent.pak %CUR_DIR%\release\nw_200_percent.pak
+if not exist %CUR_DIR%\release\nw_elf.dll copy %CUR_DIR%\buildTools\nw\nw_elf.dll %CUR_DIR%\release\nw_elf.dll
+if not exist %CUR_DIR%\release\resources.pak copy %CUR_DIR%\buildTools\nw\resources.pak %CUR_DIR%\release\resources.pak
 
 echo.
 call :ColorText 0a "Deleting temporary files..."
